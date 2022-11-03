7699c734e84db75f6a8ce4c53f743c06120899aa
pavithraM24 <pavithram@clanizon.com>
2022-11-03 14:33:18 +0530

    Added new folder

---
+++ bootstrap-5.2.2-dist/Javascript Task/jscript.js
@@ -0,0 +1,28 @@
+//Decalring two variables
+var name = 'Pavithra M'
+var age = 23
+//Declaring constants
+const address = "No: 15, Adyar, Chennai-TamilNadu"
+const phone_number = 0912456789
+//consoling those 2 variables and constants
+console.log("My Name is :", name)
+console.log("My Age is :", age)
+console.log("My address is :", address)
+console.log("Contact details :", phone_number)
+
+
+
+
+
+
+//Declaring the variable 1 in string
+var number_one = "130"
+//Converting the string variable into Number
+var number = Number("130")
+console.log("Number one is :" , number)
+console.log("Type of the number one:", typeof number)
+//Declaring the variable 2 in Number/float
+const pi_value = 3.14
+console.log("Number two is :", pi_value)
+//adding two variable
+console.log("Total is:" , number_one + pi_value)
